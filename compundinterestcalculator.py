c229b978c68edfcc095d2acbada1b659e3bea805
Shaun James <contact.shaunjames@gmail.com>
2026-03-29 21:12:18 +0400

    compundinterestcalculator.py

--- compundinterestcalculator.py
+++ compundinterestcalculator.py
@@ -1,6 +1,16 @@
-principal = int(input("Enter the principal amount: "))
-interest = float(input("Enter the interest rate (per annum) : "))
-time = int(input("Enter the time period (in years) : "))
+p = int(input("Enter the principal amount: ")) # The principal amount
+i = float(input("Enter the interest rate (per annum) : ")) # The interest rate
+t = int(input("Enter the time period (in years) : ")) # The time period
+n = int(input("Enter the number of times a year is it compounded: ")) # No. of times
+class CompoundInterest:
+  def __init__(self, p, i, t, n):
+    self.p = p
+    self.i = i
+    self.t = t
+    self.n = n
 
-
+  def calculate(self):
+    amount = self.p * (1 + self.i/self.n) ** (self.n*self.t)
+    return amount
+    
 
