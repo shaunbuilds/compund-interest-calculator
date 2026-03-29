cf084d4aca951c9b2f6bbcb97af28af93c8e949e
Shaun James <contact.shaunjames@gmail.com>
2026-03-29 17:38:34 +0400

    compundinterestcalculator.py

--- compundinterestcalculator.py
+++ compundinterestcalculator.py
@@ -2,4 +2,5 @@ principal = int(input("Enter the principal amount: "))
 interest = float(input("Enter the interest rate (per annum) : "))
 time = int(input("Enter the time period (in years) : "))
 
-amount = principal * (1 + )
+
+
